5038645099c88bd75fc6e1fae45d2e9cfb815ac1
OguzhanGuldamlasi <oguldamlasi@etu.edu.tr>
2019-09-18 11:25:40 +0300

    Js file refactored

--- validationForm.js
+++ validationForm.js
@@ -1,74 +1,69 @@
-let constButton=document.getElementById("input");
-let fakeButton=document.getElementById("customBotton");
-let buttonText=document.getElementById("span5");
-fakeButton.addEventListener("click",function () {
-    constButton.click();
-});
-constButton.addEventListener("change",function () {
-    if(constButton.value)
-        buttonText.innerText=constButton.value;
-    else
-        buttonText.innerText=constButton.value;
-});
+document.addEventListener('DOMContentLoaded', init, false);
+function init(){
+    let constButton=document.getElementById("input");
+    let fakeButton=document.getElementById("customButton");
+    let buttonText=document.getElementById("span5");
+    fakeButton.addEventListener("click",function () {
+        constButton.click();
+    });
 
-function submitCheck() {
+    constButton.addEventListener("change",function () {
+        if(constButton.value)
+            buttonText.innerText=constButton.value;
+        else
+            buttonText.innerText=constButton.value;
+    });
+}
+
+
+
+function errorMsg(name,span,errorImg) {
+    if(name==null||name==""){
+        document.getElementById(span).style.display='inline';
+        document.getElementById(errorImg).style.display='inline';
+    }
+}
+function submitCheck() {//html file has 4 spans
     let fNameInput=document.getElementById("fname");
     let fName=fNameInput.value;
-    if(fName==null||fName==""){
-        document.getElementById("span1").style.display='inline';
-        document.getElementById("errorImg1").style.display='inline';
-    }
-
+    errorMsg(fName,"span1","errorImg1");
     let lNameInput=document.getElementById("lname");
     let lName=lNameInput.value;
-    if(lName==null||lName==""){
-        document.getElementById("span2").style.display='inline';
-        document.getElementById("errorImg2").style.display='inline';
-    }
+    errorMsg(lName,"span2","errorImg2");
     let emailInp=document.getElementById("email");
     let email=emailInp.value;
-    if(!ValidateEmail(email)){
-        document.getElementById("span3").style.display='inline';
-        document.getElementById("errorImg3").style.display='inline';
+    let fNameCheck,lNameCheck,eMailCheck;
+    fNameCheck=(fName!=null&&fName!="");
+    lNameCheck=(lName!=null&&lName!="");
+    eMailCheck=validateEmail(email);
+    auxDisplay(fNameCheck&&lNameCheck&&eMailCheck,4);
+    afterSubmit(fNameCheck,lNameCheck,eMailCheck);
+    return false;//Solution against the form refresh
     }
-    let bool1,bool2,bool3;
-    bool1=(fName!=null&&fName!="");
-    bool2=(lName!=null&&lName!="");
-    if(!bool1&&!bool2){
-        document.getElementById("span4").style.display='inline';
-        document.getElementById("errorImg4").style.display='inline';
-    }
-    bool3=ValidateEmail(email);
-    if(bool1&&bool2&&bool3){
-        document.getElementById("span4").style.display='none';
-        document.getElementById("errorImg4").style.display='none';
-    }
-    afterSubmit(bool1,bool2,bool3);
-    return false;
-
-}
-function ValidateEmail(mail){
+function validateEmail(mail){
     let regexp=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (regexp.test(mail)){
+        auxDisplay(true,3)
         return true;
     }
+    auxDisplay(false,3);
     return false;
 }
-function afterSubmit(bool1,bool2,bool3){
-    if(bool1){
-        document.getElementById("span1").style.display='none';
-        document.getElementById("errorImg1").style.display='none';
-    }
-    if(bool2){
-        document.getElementById("span2").style.display='none';
-        document.getElementById("errorImg2").style.display='none';
-    }
-    if(bool3){
-        document.getElementById("span3").style.display='none';
-        document.getElementById("errorImg3").style.display='none';
-    }
-    if(bool1&&bool2&&bool3){
+function afterSubmit(fNameCheck,lNameCheck,eMailCheck){
+    auxDisplay(fNameCheck,1);
+    auxDisplay(lNameCheck,2);
+    auxDisplay(eMailCheck,3);
+    if(fNameCheck&&lNameCheck&&eMailCheck){
         location.replace("https://codepen.io/xdaniel26/full/epMZwP");
-        return false;
+    }
+}
+function auxDisplay(bool,number){
+    if(bool){
+        document.getElementById("span"+number).style.display='none';
+        document.getElementById("errorImg"+number).style.display='none';
+    }
+    else{
+        document.getElementById("span"+number).style.display='inline';
+        document.getElementById("errorImg"+number).style.display='inline';
     }
 }
